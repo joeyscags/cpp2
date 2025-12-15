9792f732c484d04d6d8b9e9daeb42263471c06d2
joeyscags <joeyscags5000@gmail.com>
2025-12-15 18:25:56 +0100

    copied basic structure without the main from ex01  before implementing the new tasks for ex02

---
+++ ex02/Fixed.cpp
@@ -0,0 +1,60 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	_fixedPointValue = 0;
+}
+
+Fixed::Fixed(const int intValue)
+{
+	_fixedPointValue = intValue << _fractionalBits;
+}
+
+Fixed::Fixed(const float floatValue)
+{
+	_fixedPointValue = roundf(floatValue * (1 << _fractionalBits));
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+	_fixedPointValue = other._fixedPointValue;
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+	if (this != &other)
+	{
+		_fixedPointValue = other._fixedPointValue;
+	}
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+}
+
+int Fixed::getRawBits(void) const
+{
+	return (_fixedPointValue);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	_fixedPointValue = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+	return ((float)_fixedPointValue / (1 << _fractionalBits));
+}
+
+int Fixed::toInt(void) const
+{
+	return (_fixedPointValue >> _fractionalBits);
+}
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed)
+{
+	out << fixed.toFloat();
+	return (out);
+}
