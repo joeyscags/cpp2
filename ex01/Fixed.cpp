2ac7f73afe5832c6aa1f6f8060901bb428769cb2
joeyscags <joeyscags5000@gmail.com>
2025-12-15 15:02:04 +0100

    just literally copied the basic structure from ex00 into ex01 since it seems awfully the same

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,38 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	_fixedPointValue = 0;
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	_fixedPointValue = other._fixedPointValue;
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &other)
+	{
+		_fixedPointValue = other._fixedPointValue;
+	}
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+	return (_fixedPointValue);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	_fixedPointValue = raw;
+}
