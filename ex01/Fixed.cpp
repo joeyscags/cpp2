71fc91b2055eadb9c47012c24b2cfa969ad2e4c2
joeyscags <joeyscags5000@gmail.com>
2025-12-15 18:09:51 +0100

    pretty easy exercise since it was just building on the concept we just learned added 2 fixed constructor for both a integer and a float that takes both number and multiplies them by 256 and then have a toInt / float function that does the opposite for when i want to retrieve the value. Added a << operator so that i can display the fixed values more easily into the standard out and added  main to test

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,6 +6,18 @@ Fixed::Fixed()
 	_fixedPointValue = 0;
 }
 
+Fixed::Fixed(const int intValue)
+{
+	std::cout << "Int constructor called" << std::endl;
+	_fixedPointValue = intValue << _fractionalBits;
+}
+
+Fixed::Fixed(const float floatValue)
+{
+	std::cout << "Float constructor called" << std::endl;
+	_fixedPointValue = roundf(floatValue * (1 << _fractionalBits));
+}
+
 Fixed::Fixed(const Fixed &other)
 {
 	std::cout << "Copy constructor called" << std::endl;
@@ -35,4 +47,20 @@ int Fixed::getRawBits(void) const
 void Fixed::setRawBits(int const raw)
 {
 	_fixedPointValue = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+	return ((float)_fixedPointValue / (1 << _fractionalBits));
+}
+
+int Fixed::toInt(void) const
+{
+	return (_fixedPointValue >> _fractionalBits);
+}
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed)
+{
+	out << fixed.toFloat();
+	return (out);
 }
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -1,6 +1,7 @@
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
+# include <cmath>
 # include <iostream>
 
 class Fixed
@@ -11,12 +12,18 @@ class Fixed
 
   public:
 	Fixed();
+	Fixed(const int intValue);
+	Fixed(const float floatValue);
 	Fixed(const Fixed &other);
 	Fixed &operator=(const Fixed &other);
 	~Fixed();
 
 	int getRawBits(void) const;
 	void setRawBits(int const raw);
+	float toFloat(void) const;
+	int toInt(void) const;
 };
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed);
 
 #endif
